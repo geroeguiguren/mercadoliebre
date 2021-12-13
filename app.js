8832d3a3a33d535781ed43bd5b8d9e5d844ef0e8
geroeguiguren <geroeguiguren@gmail.com>
2021-12-13 14:59:12 -0300

    Mercadoliebre finalizado

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require ("path");
 
 const app = express ();
 
-app.listen (8000, () => console.log ("Servidor corriendo en el puerto 8000"));
+app.listen (3000, () => console.log ("Servidor corriendo en el puerto 3000"));
 
 app.use (express.static(path.join (__dirname, "public")))
 
@@ -16,4 +16,9 @@ app.get ("/", (req,res) => {
 app.get ("/register", (req,res) => {
 
     res.sendFile (path.join(__dirname, "views/register.html"))
+})
+
+app.get ("/login", (req,res) => {
+
+    res.sendFile (path.join(__dirname, "views/login.html"))
 })
