1fc12856a9f5cc8b9dd947e1099d6530dbb9d93a
geroeguiguren <geroeguiguren@gmail.com>
2021-12-13 22:03:17 -0300

    heroku

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const path = require ("path");
 
 const app = express ();
 
-app.listen (3000, () => console.log ("Servidor corriendo en el puerto 3000"));
+app.listen (process.env.PORT || 3000, function () {
+     console.log ("Servidor corriendo en el puerto 3000")
+});
 
 app.use (express.static(path.join (__dirname, "public")))
 
